ab7147ec4b9fd2b2ca20fae59068e08c707bec5b
pipito-yukio <33173429+pipito-yukio@users.noreply.github.com>
2022-05-17 11:14:34 +0900

    起動時にホスト名が取得できない場合の対応。リトライ処理追加。

--- flask_web/src/run.py
+++ flask_web/src/run.py
@@ -1,4 +1,5 @@
 import os
+import time
 from weather_finder import app, app_logger
 
 """
@@ -18,7 +19,28 @@ if __name__ == '__main__':
             # Prerequisites: pip install waitress
             from waitress import serve
             app_logger.info("Production start.")
-            serve(app, host=host, port=port)
+            retry = 0
+            while True:
+                try:
+                    serve(app, host=host, port=port)
+                    # Not raise OSError 
+                    break
+                except OSError as oserr:
+                    # 16:41:57 xxxxx systemd[1]: Started Flask webapp WeatherFinder service.
+                    # ...
+                    # 16:42:03 xxxxx avahi-daemon[419]: Registering new address record for ... on eth0.*.
+                    # 16:42:03 xxxxx dhcpcd[420]: eth0: soliciting an IPv6 router
+                    # 16:42:03 xxxxx start.sh[397]: ERROR:app_main:[Errno 99] Cannot assign requested address
+                    # ...
+                    # 16:42:04 xxxxx systemd-networkd[157]: eth0: Gained IPv6LL
+                    # 16:42:04 xxxxx systemd-timesyncd[349]: Network configuration changed, trying to establish connection.
+                    retry += 1
+                    app_logger.error(oserr)
+                    if retry <= 3:
+                        time.sleep(5.0) # if time.sleep(3.0) -> retry:2
+                        app_logger.warning("Sleep 5 seconds after retry:{}.".format(retry))
+                    else:
+                        raise oserr 
         except ImportError:
             # Production with flask,debug False
             app_logger.info("Development start, without debug.")
