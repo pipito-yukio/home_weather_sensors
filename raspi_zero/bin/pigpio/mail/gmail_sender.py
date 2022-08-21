7660c348768e55ed8741180e29efc720805c8fe7
pipito-yukio <33173429+pipito-yukio@users.noreply.github.com>
2022-08-21 15:10:24 +0900

    Improved email sending process

--- raspi_zero/bin/pigpio/mail/gmail_sender.py
+++ raspi_zero/bin/pigpio/mail/gmail_sender.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import smtplib
 
@@ -5,32 +6,50 @@ import smtplib
 # https://www.bc-robotics.com/tutorials/sending-email-using-python-raspberry-pi/
 # Sending An Email Using Python On The Raspberry Pi
 
-GMAIL_USERNAME = os.environ.get("GMAIL_USER")
-
 
 class GMailer:
-    SMTP_SERVER = "smtp.gmail.com"
-    SMTP_PORT = 587
+    _SMTP_SERVER = "smtp.gmail.com"
+    _SMTP_PORT = 587
     _GMAIL_PASSWD = os.environ.get("GMAIL_PASSWD")
+    GMAIL_USER = os.environ.get("GMAIL_USER")
 
-    def sendmail(self, recipient, subject, content, event=None):
-        # Create Headers
-        headers = ["From: " + GMAIL_USERNAME, "Subject: " + subject, "To: " + recipient,
+    def __init__(self, logger=None):
+        self.logger = logger
+        if self.logger is not None:
+            self.logger_debug = (self.logger.getEffectiveLevel() <= logging.DEBUG)
+        else:
+            self.logger_debug = False
+
+    def sendmail(self, subject, content, recipients, event=None):
+        # Create Headers:
+        if recipients is not None:
+            if len(recipients) > 0:
+                # https://docs.python.org/ja/3/library/smtplib.html: SMTP 使用例
+                to_addrs = ",".join(recipients)
+            else:
+                to_addrs = self.GMAIL_USER
+        else:
+            to_addrs = self.GMAIL_USER
+        headers = ["From: " + self.GMAIL_USER, "Subject: " + subject, "To: " + to_addrs,
                    "MIME-Version: 1.0", "Content-Type: text/plain"]
         headers = "\r\n".join(headers)
+        if self.logger_debug:
+            self.logger.debug("headers: {}".format(headers))
         # Connection to Gmail server
-        session = smtplib.SMTP(self.SMTP_SERVER, self.SMTP_PORT)
+        session = smtplib.SMTP(self._SMTP_SERVER, self._SMTP_PORT)
         session.ehlo()
         session.starttls()
         session.ehlo()
 
         # Login
-        session.login(GMAIL_USERNAME, self._GMAIL_PASSWD)
+        session.login(self.GMAIL_USER, self._GMAIL_PASSWD)
 
         # Send mail and Exit
         header_content = headers + "\r\n\r\n" + content
         header_content = header_content.encode("utf8")
-        session.sendmail(GMAIL_USERNAME, recipient, header_content)
+        if self.logger_debug:
+            self.logger.debug("header_content: {}".format(header_content))
+        session.sendmail(self.GMAIL_USER, to_addrs, header_content)
         session.quit()
         if event is not None:
             event.set()
